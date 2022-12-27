96a9ce5c51e5a8d5177b716193018e5d2e608459
JohanCode2180100 <Johan_CODE21@orange.fr>
2022-12-27 12:13:09 +0100

    Add JS for a navbar responsive - it's remains a debug a responsive navbar

---
+++ app.js
@@ -0,0 +1,26 @@
+//Menu responsive
+
+const logoMenu = document.querySelector(".logoMenuImg");
+const contItems = document.querySelector(".contItems");
+const arrItems = document.querySelectorAll(".Items");
+
+logoMenu.addEventListener("click", () => {
+  if (contItems.className === "contItems") {
+    contItems.className += "responsive";
+    logoMenu.style.width = "30px";
+    logoMenu.style.top = "5px";
+    logoMenu.src = "assets/pictures/close.svg";
+  } else {
+    contItems.className = "contItems";
+    logoMenu.style.width = "30px";
+    logoMenu.style.top = "13px";
+    logoMenu.src = "assets/pictures/menu.svg";
+  }
+});
+
+arrItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    contItems.className = "contItems";
+    logoMenu.src = "assets/pictures/menu.svg";
+  });
+});
